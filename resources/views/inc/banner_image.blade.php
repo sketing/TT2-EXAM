60ab7aa44fd63528d75acd3023c313f2e6cd3795
Mikus <mikus.kalnins144@gmail.com>
2018-06-07 18:07:58 +0300

    working with basket and templates

---
+++ resources/views/inc/banner_image.blade.php
@@ -0,0 +1,6 @@
+@section('banner_image')
+        <div class="sidebar">
+            <h3>Banner_image</h3>
+            BANNER_IMAGE
+            @show
+        </div>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>StarWars</title>
+    </head>
+    <body>
+        <!-- TOP PART-->
+        <div id='site_top'>
+            <div id='logo'>@include('inc.logo')</div>
+            <div id='banner'>
+                <div id='banner_image'>@include('inc.banner_image')</div>
+                <div id='banner_menu'>
+                    <div id='left'></div>
+                    <div id='right'></div>
+                </div>
+            </div>
+        </div>
+        
+        <!-- MAIN PART -->
+        <div id='main'>
+            <div id='sidebar'>@include('inc.sidebar')</div>
+            <div id='main'>
+                <div id='top_menu'>
+                    <div id='left'></div>
+                    <div id='right'></div>
+                </div>
+                <div id='main_part'>@yield('content')</div>
+            </div>
+        </div>
+        
+        <!-- BOTTOM PART-->    
+        
+        @include('inc.sidebar')
+        
+    </body>
+</html>
+
