893b9a791f162eb962fbef41e30e1613fbc4040a
Mikus <mikus.kalnins144@gmail.com>
2018-06-10 14:52:01 +0300

    lieku vēl jopromāj klāt sidebar, spēlējos

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,5 +1,6 @@
+<!-- NAVBAR -->
 <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-<a class="navbar-brand" href="/">{{config('app.name', 'TT2-EXAM')}}</a>
+    <a class="navbar-brand" href="/">{{config('app.name', 'TT2-EXAM')}}</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,45 +1,54 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-US-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="{{asset('css/app.css')}}">
-        <!-- zemāk ir visi fucking, fucking, fucking...-->
-        <script src={{ asset("js/app.js") }}></script>
-        <title>StarWars</title>
-    </head>
-    <body>
-        <!-- TOP PART-->
-        <div id='site_top'>
-            <div id='logo'>@include('inc.logo')</div>
-            <div id='banner'>
-                <div id='banner_image'>@include('inc.banner_image')</div>
-                <div id='banner_menu'>
-                    <div id='left'></div>
-                    <div id='right'></div>
+<html lang="en">
+<head>
+    <!-- CSS -->
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <!-- JS, jquery etc -->
+    <script src={{ asset("js/app.js") }}></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+    <title>Dashboard Template for Bootstrap</title>
+
+    <style type="text/css">/* Chart.js */
+    @-webkit-keyframes chartjs-render-animation{from{opacity:0.99}to{opacity:1}}@keyframes chartjs-render-animation{from{opacity:0.99}to{opacity:1}}.chartjs-render-monitor{-webkit-animation:chartjs-render-animation 0.001s;animation:chartjs-render-animation 0.001s;}</style></head>
+    
+      <body>
+        @include('inc.navbar')    
+        <div class="container-fluid">
+          <div class="row">
+            @include('inc.sidebar')
+            <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4"><div style="position: absolute; left: 0px; top: 0px; right: 0px; bottom: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;" class="chartjs-size-monitor"><div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div></div><div class="chartjs-size-monitor-shrink" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:200%;height:200%;left:0; top:0"></div></div></div>
+              <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                    @yield('content')
+                    <!--
+                    <h1 class="h2">Dashboard</h1>
+                    <div class="btn-toolbar mb-2 mb-md-0">
+                      <div class="btn-group mr-2">
+                        <button class="btn btn-sm btn-outline-secondary">Share</button>
+                        <button class="btn btn-sm btn-outline-secondary">Export</button>
+                      </div>
+                      <button class="btn btn-sm btn-outline-secondary dropdown-toggle">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
+                        This week
+                      </button>
+                    -->
                 </div>
-            </div>
         </div>
-        
-        <!-- MAIN PART -->
-        <div id='main'>
-            <div id='sidebar'>@include('inc.sidebar')</div>
-            <div id='main'>
-                <div id='top_menu'>
-                    <!-- WIP -->
-                    @include('inc.navbar')
-                    <!-- WIP -->
-                    <div id='left'></div>
-                    <div id='right'></div>
-                </div>
-                <div id='main_part' class="container">@yield('content')</div>
-            </div>
-        </div>
-        
-        <!-- BOTTOM PART-->    
-        
-        @include('inc.sidebar')
     </body>
 </html>
-
+<!--
+@yield('content')
+<h1 class="h2">Dashboard</h1>
+<div class="btn-toolbar mb-2 mb-md-0">
+    <div class="btn-group mr-2">
+    <button class="btn btn-sm btn-outline-secondary">Share</button>
+    <button class="btn btn-sm btn-outline-secondary">Export</button>
+    </div>
+    <button class="btn btn-sm btn-outline-secondary dropdown-toggle">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
+    This week
+    </button>
+-->
