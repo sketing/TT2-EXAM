2b0df8b7fa3819398579fc5ff7d479cf19947f68
Rihards K <rihards.krastins14@gmail.com>
2018-06-17 12:07:58 +0300

    Made Database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,23 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
+            $table->increments('user_id');
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('name');
+            $table->string('surname');
+            $table->date('dob');
+            $table->unsignedInteger('adress_id');      
+            $table->foreign('adress_id')->references('adress_id')->on('adresses');
+            $table->unsignedInteger('credit_card_id');
+            $table->foreign('credit_card_id')->references('credit_card_id')->on('credit_cards');
+            $table->unsignedInteger('paypal_id');
+            $table->foreign('paypal_id')->references('paypal_id')->on('paypals');
+            $table->boolean('news_letter_checkbox');
+            $table->string('role')->default('customer');
             $table->rememberToken();
-            $table->timestamps();
+            $table->timestamps()
         });
     }
 
---
+++ database/migrations/2018_06_17_090213_create_paypals_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePaypalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('paypals', function (Blueprint $table) {
+            $table->increments('credit_card_id');
+            $table->char('credit_card_number', 16);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('paypals');
+    }
+}
